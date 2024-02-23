410ace80959b2ab367b4e2f279acd08e5bd3fc91
NitheshGowda16 <93904719+NitheshGowda16@users.noreply.github.com>
2024-02-23 14:54:19 -0700

    Update index.js

--- final_project/index.js
+++ final_project/index.js
@@ -3,7 +3,6 @@ const jwt = require('jsonwebtoken');
 const session = require('express-session')
 const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
-const jwtSecret = '244d0b97c61cb978567e348a15fc8cd5c3c5791af982ccae88db48383bc3c273';
 
 const app = express();
 
@@ -12,26 +11,24 @@ app.use(express.json());
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-    const authHeader = req.headers.authorization;
-    if (!authHeader) {
-        return res.status(401).json({ message: "No token provided" });
+//Write the authenication mechanism here
+    if(req.session.authorization) {
+        token = req.session.authorization['accessToken'];
+        jwt.verify(token, "access",(err,user)=>{
+            if(!err){
+                req.user = user;
+                next();
+            }
+            else{
+                return res.status(403).json({message: "User not authenticated"})
+            }
+        });
+    } else {
+        return res.status(403).json({message: "User not logged in"})
     }
-
-    const token = authHeader.split(' ')[1]; // Bearer <token>
-
-    // Verify the token
-    jwt.verify(token, jwtSecret, (err, decoded) => {
-        if (err) {
-            return res.status(401).json({ message: "Invalid token" });
-        }
-
-        // If token is valid, set the decoded user object to the request for use in other routes
-        req.user = decoded;
-        next(); // proceed to the next middleware or route handler
     });
-});
  
-const PORT =5000;
+const PORT =4000;
 
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
